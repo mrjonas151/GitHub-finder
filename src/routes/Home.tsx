81f1de03ca7154513508c3b46f71460e8fa0a385
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-08-03 13:37:44 -0300

    feat: adding loader component to load the requisition of api

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,16 +3,20 @@ import { useState } from "react"
 import { UserProps } from "../types/user";
 import User from "../components/User";
 import Error from "../components/Error";
+import Loader from "../components/Loader";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const loadUser = async (username: string) => {
+    setLoading(true);
     setError(false);
     setUser(null);
     const response = await fetch(`https://api.github.com/users/${username}`);
     const data = await response.json();
+    setLoading(false);
 
     if(response.status === 404) {
       setError(true);
@@ -34,6 +38,7 @@ const Home = () => {
   return (
     <div>
         <Search loadUser={loadUser} />
+        {loading && <Loader />}
         {user && <User {...user} />}
         {error && <Error />}
     </div>
