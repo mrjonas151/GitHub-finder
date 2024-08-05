b6ef497f0230c1b69a45cd2fe2bbaaa013c0ad4a
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-08-05 14:07:54 -0300

    feat: bringing repos data from github api

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -2,9 +2,27 @@ import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import styles from "./Repos.module.css"
 import BackBtn from "../components/BackBtn"
+import { RepoProps } from "../types/repo"
+import Loader from "../components/Loader"
 
 const Repos = () => {
-    const { username } = useParams()
+    const { username } = useParams();
+    const [repos, setRepos] = useState<RepoProps[] | [] | null>([]);
+    const [isloading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        
+        const loadRepos = async function (username: string = "") {
+            setIsLoading(true);
+            const response = await fetch(`https://api.github.com/users/${username}/repos`);
+            const data = await response.json();
+
+            setRepos(data);
+            setIsLoading(false);
+        };
+        loadRepos(username);
+    }, []);
+
 
   return (
     <div>
